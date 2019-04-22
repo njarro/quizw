26296a9697808555732e1c957bf7d9b1af0609c8
Estudiante <Estudiante@BE401S103C07ING.unal.loc>
2019-04-22 07:54:56 -0500

    cambios1

---
+++ src/ejerciciorectangulo/Main.java
@@ -0,0 +1,13 @@
+
+package ejerciciorectangulo;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Rectangulo r1 = new Rectangulo( 3, 4 );
+        System.out.println( "Perimetro: " + r1.calcularPerimetro() );
+        System.out.println( "Area: " + r1.calcularArea() );
+    }
+    
+}
