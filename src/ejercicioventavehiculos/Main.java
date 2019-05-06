9b08723f21b028adbe593c6e31903a1cbf07ecb0
Estudiante <Estudiante@BE401S103C07ING.unal.loc>
2019-05-06 09:10:50 -0500

    Ejercicio Venta Vehiculos v3 con apuntes

--- src/ejercicioventavehiculos/Main.java
+++ src/ejercicioventavehiculos/Main.java
@@ -6,11 +6,15 @@ import java.util.HashMap;
 public class Main {
     public static void main(String args[]) {
         Empresa empresa = new Empresa();
-        Auto auto_lujo = new AutoLujo(4, "AHG-453", 4567, "Lamborghinni", 2018, 870000000);
-        Auto auto_compacto = new AutoCompacto(3, "JGH-765", 3423, "Renault", 1997, 3400000);
+        Auto auto_lujo1 = new AutoLujo(4, "AHG-453", 4567, "Lamborghini", 2018, 870000000);
+        Auto auto_lujo2 = new AutoLujo(4, "BJH-565", 5654, "Ferrari", 2018, 540000000);
+        Auto auto_compacto1 = new AutoCompacto(3, "JGH-765", 3423, "Renault", 1997, 3400000);
+        Auto auto_compacto2 = new AutoCompacto(2, "YUT-657", 7860, "Chevrolet", 1984, 1200000);
         
-        empresa.registrarAuto(auto_lujo);
-        empresa.registrarAuto(auto_compacto);
+        empresa.registrarAuto(auto_lujo1);
+        empresa.registrarAuto(auto_lujo2);
+        empresa.registrarAuto(auto_compacto1);
+        empresa.registrarAuto(auto_compacto2);
         
         HashMap<String, Auto> autos = empresa.listarAutos();
         
